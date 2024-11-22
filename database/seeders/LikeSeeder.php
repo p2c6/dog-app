2d0daf31f2e65590eec5527a6257fccdc5147c4c
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-22 16:02:28 +0800

    feat: add dummy like data

--- database/seeders/LikeSeeder.php
+++ database/seeders/LikeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Like;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,33 @@ class LikeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $likes = [
+            [
+                'id' => 1,
+                'user_id' => 1,
+                'dog_id' => 1,
+                'created_at' => now()
+            ],
+            [
+                'id' => 2,
+                'user_id' => 2,
+                'dog_id' => 2,
+                'created_at' => now()
+            ],
+            [
+                'id' => 3,
+                'user_id' => 2,
+                'dog_id' => 3,
+                'created_at' => now()
+            ],
+            [
+                'id' => 4,
+                'user_id' => 2,
+                'dog_id' => 4,
+                'created_at' => now()
+            ],
+        ];
+
+        Like::insert($likes);
     }
 }
