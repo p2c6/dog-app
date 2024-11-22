290898dd7e4a01f430e8b870d6164b4ae71c256c
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-22 13:49:59 +0800

    feat: update redirection entrypoint

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function() {
     return redirect('dashboard');
-});
+})->name('home');;
 
 Route::get('/dashboard', Dashboard::class)
     ->name('dashboard')
