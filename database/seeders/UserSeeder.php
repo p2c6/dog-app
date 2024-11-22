d2b9941b18ae9bff1432e1bbc83b5052d9256fdd
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-22 15:58:19 +0800

    feat: add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Profile;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'id' => 1,
+                'email' => 'jdoe@test.com',
+                'password' => bcrypt('password'),
+                'email_verified_at' => now(),
+                'created_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'email' => 'jcarey@test.com',
+                'password' => bcrypt('password'),
+                'email_verified_at' => now(),
+                'created_at' => now(),
+            ],
+            [
+                'id' => 3,
+                'email' => 'kmarx@test.com',
+                'password' => bcrypt('password'),
+                'email_verified_at' => now(),
+                'created_at' => now(),
+            ],
+        ];
+
+        User::insert($users);
+
+        $profiles = [
+            [
+                'id' => 1,
+                'user_id' => 1,
+                'name' => 'John Doe',
+                'address' => 'Quezon City',
+                'dob' => '1995-05-06',
+                'gender' => 'Male',
+                'created_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'user_id' => 2,
+                'name' => 'Janna Carey',
+                'address' => 'Paranaque City',
+                'dob' => '1995-06-08',
+                'gender' => 'Female',
+                'created_at' => now(),
+            ],
+            [
+                'id' => 3,
+                'user_id' => 3,
+                'name' => 'Karl Marx',
+                'address' => 'Cebu City',
+                'dob' => '1992-04-08',
+                'gender' => 'Male',
+                'created_at' => now(),
+            ],
+        ];
+
+        Profile::insert($profiles);
+    }
+}
