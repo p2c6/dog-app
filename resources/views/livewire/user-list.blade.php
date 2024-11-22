f7d217e6e70eb5a587d931308c8c13e83679f952
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-22 16:06:01 +0800

    chore: update text

--- resources/views/livewire/user-list.blade.php
+++ resources/views/livewire/user-list.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col">
-    <h4 class="self-center my-5 text-xs md:text-md text-gray">Other users who might like dog also</h4>
+    <h4 class="self-center my-5 text-xs md:text-md text-gray">Other users who might like dogs also</h4>
     <div class="border-b-2 border-gray-200"></div>
     <div>
         @forelse ( $users as $user )
