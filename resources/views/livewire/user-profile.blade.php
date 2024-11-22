a4824887d112b28e314d5d79dfd54aac7514a382
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-22 15:45:47 +0800

    feat: add conditional ui

--- resources/views/livewire/user-profile.blade.php
+++ resources/views/livewire/user-profile.blade.php
@@ -5,9 +5,23 @@
     <div class="flex flex-col md:flex-row gap-5">
         <div>
             <div class="bg-white w-auto md:w-96 h-48 mt-5">
-                <div class="flex flex-col items-center justify-center">
-                    <img class="rounded-full w-16 h-16" src="https://i.pravatar.cc/150?img=8" alt="">
-                    <h4 class="self-center my-5 text-xs md:text-md text-gray"> {{ $user['name'] }}</h4>
+                <div class="flex items-center justify-center mt-10">
+                    <div class="flex flex-col items-center justify-center mt-2">
+                        <div class="bg-teal-500 rounded-full w-10 h-10 pt-2">
+                            <p class="text-xl text-white flex items-center justify-center font-bold">{{ $user['name'][0] }}</p>
+                        </div>
+                        <h4 class="self-center mt-5 text-xs md:text-md text-blue-600 font-semibold"> {{ $user['name'] }}</h4>
+                        @if($user['description'])
+                            <div class="w-48 text-center">
+                                <p class="text-gray-300 text-xs">{{ $user['description'] }}</p>
+                            </div>
+                        @endif
+                        @if($user['address'])
+                            <div>
+                                <p class="text-gray-300 text-xs"><i class="fa-solid fa-location-dot"></i> {{ $user['address'] }}</p>
+                            </div>
+                        @endif
+                    </div>
                 </div>
                 
             </div>
