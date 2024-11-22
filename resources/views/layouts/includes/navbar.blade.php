552594144dab0118a8ea78de8ca93ab9896820c8
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-22 13:27:54 +0800

    feat: update fetch name implementation

--- resources/views/layouts/includes/navbar.blade.php
+++ resources/views/layouts/includes/navbar.blade.php
@@ -7,7 +7,7 @@
             <div class="relative inline-block text-left" x-data="{ collapse: false }">
                 <div class="hover:cursor-pointer">
                     <a @click="collapse = !collapse" class="inline-flex w-full justify-center gap-x-1.5 px-3 py-2 text-sm font-semibold" id="menu-button" aria-expanded="true" aria-haspopup="true">
-                    {{ auth()->user()->name }}
+                    {{ auth()->user()->profile->name }}
                     <svg class="-mr-1 size-5 text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                         <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
                     </svg>
