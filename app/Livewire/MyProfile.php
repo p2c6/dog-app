fd145734bde9d2bd50c9f568442d2c6b59809f07
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-22 13:26:31 +0800

    feat: implement update profile; implement update about me

--- app/Livewire/MyProfile.php
+++ app/Livewire/MyProfile.php
@@ -8,13 +8,55 @@ class MyProfile extends Component
 {
     public $profile;
 
+    public $name = '';
+
+    public $address = '';
+
+    public $dob = '';
+
+    public $gender = '';
+
+    public $description = '';
+
+
     public function mount()
     {
-        $this->profile = auth()->user();
+        $user = auth()->user();
+        $this->profile = $user->profile;
+        $this->name = $this->profile->name;
+        $this->address = $this->profile->address;
+        $this->dob = $this->profile->dob;
+        $this->gender = $this->profile->gender;
+        $this->description = $this->profile->description;
+    }
+
+    public function updateProfile()
+    {
+        $this->validate([
+            'name' => ['required'],
+        ]);
+
+        auth()->user()->profile()->update([
+            'name' => $this->name,
+            'address' => $this->address,
+            'dob' => $this->dob,
+            'gender' => $this->gender,
+        ]);
+
+        return redirect('my-profile')->with('message', 'Successfully Updated Profile.');
+    }
+
+    public function updateAboutMe()
+    {
+        auth()->user()->profile()->update([
+            'description' => $this->description,
+        ]);
+
+        return redirect('my-profile')->with('message', 'Successfully Updated About Me.');
     }
 
     public function render()
     {
-        return view('livewire.my-profile')->layout('layouts.base');
+        return view('livewire.my-profile')->layout('layouts.app');
     }
 }
