5cc1458414c84bfdfec704e6d7416b1461ae1738
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-22 15:45:13 +0800

    feat: add conditional ui

--- resources/views/livewire/like-list.blade.php
+++ resources/views/livewire/like-list.blade.php
@@ -1,5 +1,12 @@
 <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
-    @foreach($dogs as $dog)
+    @forelse ($dogs as $dog )
         @livewire('like', ['dog' => $dog], key($dog['breed']))
-    @endforeach
+    @empty
+    <div class="w-96 rounded-md">
+        <div class="flex flex-col items-center">
+            No data found.
+        </div>
+    </div>
+    @endforelse
+
 </div>
