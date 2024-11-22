290fee53ef837111a8da5b2fc2514f5f0d55ddb6
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-22 13:38:39 +0800

    feat: add ui for empty user

--- resources/views/livewire/user-list.blade.php
+++ resources/views/livewire/user-list.blade.php
@@ -2,8 +2,13 @@
     <h4 class="self-center my-5 text-xs md:text-md text-gray">Other users who might like dog also</h4>
     <div class="border-b-2 border-gray-200"></div>
     <div>
-        @foreach($users as $user)
+        @forelse ( $users as $user )
             @livewire('show-user', ['user' => $user])
-        @endforeach
+        @empty
+        <div class="flex flex-col items-center justify-center mt-20">
+            <i class="fa-regular fa-face-frown-open text-4xl text-gray-400"></i>
+            <p class="text-gray-400 text-sm">No user/s found</p>
+        </div>
+        @endforelse
     </div>
 </div>
