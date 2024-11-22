24f388ad0869fd406ba07bc99d5f61be042bd856
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-22 13:31:28 +0800

    feat: add initial name mark

--- resources/views/livewire/show-user.blade.php
+++ resources/views/livewire/show-user.blade.php
@@ -1,4 +1,6 @@
-<div class="flex flex-row gap-1 px-5 py-0 mt-2">
-    <img class="rounded-full w-5 h-5" src="https://i.pravatar.cc/150?img=8" alt="avatar">
-    <a href="{{ route('user-profile', $user) }}" class="text-xs text-blue-900 hover:cursor-pointer">{{ $user['name'] }}</a>
+<div class="flex flex-row gap-1 px-5 py-0 mt-2 items-center">
+    <div class="bg-teal-500 rounded-full w-6 h-6 pt-1">
+        <p class="text-xs text-white flex items-center justify-center font-bold">{{ $user['name'][0] }}</p>
+    </div>
+    <a href="{{ route('user-profile', $user['user_id']) }}" class="text-xs text-blue-900 hover:cursor-pointer">{{ $user['name'] }}</a>
 </div>
